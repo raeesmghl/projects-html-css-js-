a52fce772fb46dfec8b81697c6fbcb3d5cf7aa7b
Raees Mughal <147074329+raeesmghl@users.noreply.github.com>
2024-05-25 22:44:34 +0500

    Update script.js

--- slider/script.js
+++ slider/script.js
@@ -1,1 +1,15 @@
 
+let imageIndex = 0;
+let func = ()=>{
+    let images = document.querySelectorAll('.image');
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.display = 'none'
+    }
+    imageIndex++;
+    if(imageIndex> images.length){
+        imageIndex = 1;
+    };
+    images[imageIndex-1].style.display = 'block'
+};
+setInterval(func,3000)
+
